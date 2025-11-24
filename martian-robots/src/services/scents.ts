5eca8796adaa4b57aeb8a1a4ab6e2740c6b9e615
LameesAjouhaar <lameesajouhaar@gmail.com>
2025-11-24 09:26:05 +0200

    Direction logic implemented

---
+++ martian-robots/src/services/scents.ts
@@ -0,0 +1,7 @@
+const scents = new Set<string>();
+
+export const hasScent = (x: number, y: number) =>
+  scents.has(`${x},${y}`);
+
+export const addScent = (x: number, y: number) =>
+  scents.add(`${x},${y}`);
